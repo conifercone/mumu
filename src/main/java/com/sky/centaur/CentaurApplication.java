6e93f567563af4db957c752e4b726b72ba575bf8
单开宇 <kaiyu.shan@outlook.com>
2024-01-09 14:19:31 +0800

    Initial commit

---
+++ src/main/java/com/sky/centaur/CentaurApplication.java
@@ -0,0 +1,13 @@
+package com.sky.centaur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentaurApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CentaurApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sky/centaur/CentaurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.centaur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentaurApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
