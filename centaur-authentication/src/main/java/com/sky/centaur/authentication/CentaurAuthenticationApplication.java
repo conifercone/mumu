33062d6a7bb186e9ed503800c20e47d268cf995a
单开宇 <kaiyu.shan@outlook.com>
2024-01-09 15:55:31 +0800

    feat(project): 新增centaur-authentication模块

---
+++ centaur-authentication/src/main/java/com/sky/centaur/authentication/CentaurAuthenticationApplication.java
@@ -0,0 +1,18 @@
+package com.sky.centaur.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 鉴权服务
+ *
+ * @author 单开宇
+ * @since 2024-01-09
+ */
+@SpringBootApplication
+public class CentaurAuthenticationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CentaurAuthenticationApplication.class, args);
+  }
+}
