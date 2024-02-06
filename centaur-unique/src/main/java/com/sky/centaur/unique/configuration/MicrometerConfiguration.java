1bc7ad8ce802289f5fc3b739cd5ca2b57ee88fc3
单开宇 <kaiyu.shan@outlook.com>
2024-02-06 17:17:34 +0800

    feat(project): 集成prometheus

--- centaur-unique/src/main/java/com/sky/centaur/unique/configuration/MicrometerConfiguration.java
+++ centaur-unique/src/main/java/com/sky/centaur/unique/configuration/MicrometerConfiguration.java
@@ -16,9 +16,12 @@
 
 package com.sky.centaur.unique.configuration;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.grpc.ObservationGrpcClientInterceptor;
 import io.micrometer.core.instrument.binder.grpc.ObservationGrpcServerInterceptor;
 import io.micrometer.observation.ObservationRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -41,5 +44,11 @@ public class MicrometerConfiguration {
   public ObservationGrpcServerInterceptor observationGrpcServerInterceptor(
       ObservationRegistry observationRegistry) {
     return new ObservationGrpcServerInterceptor(observationRegistry);
+  }
+
+  @Bean
+  MeterRegistryCustomizer<MeterRegistry> configurer(
+      @Value("${spring.application.name}") String applicationName) {
+    return registry -> registry.config().commonTags("application", applicationName);
   }
 }
