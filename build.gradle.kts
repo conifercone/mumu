57a670421c29584090640dcc356919e822184f3d
kaiyu.shan <kaiyu.shan@outlook.com>
2024-11-11 11:22:35 +0800

    build(git): pre-commit增加自动更新license

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,15 @@ tasks.register<Copy>("installGitHooks") {
     // 源文件路径
     val hooksDir = file("${project.rootDir}/.git/hooks")
     val sourceDir = file("${project.rootDir}/scripts/git/hooks")
+    val updateLicenseShell = file("${project.rootDir}/update_license.sh")
     // 将文件从源目录拷贝到目标目录
     from(sourceDir)
     // 目标目录
     into(hooksDir)
     // 设置执行权限（可选）
     doLast {
+        // 设置 update_license.sh 的执行权限
+        updateLicenseShell.setExecutable(true)
         // 设置 pre-commit 的执行权限
         hooksDir.resolve("pre-commit").setExecutable(true)
         // 设置 commit-msg 的执行权限
