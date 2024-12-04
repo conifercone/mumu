2147f142f9879c204ddb904baa208aa52fc51211
kaiyu.shan <kaiyu.shan@outlook.com>
2024-12-04 17:04:23 +0800

    refactor(mumu): 简化代码

--- mumu-basis/src/main/java/baby/mumu/basis/mappers/BaseMapper.java
+++ mumu-basis/src/main/java/baby/mumu/basis/mappers/BaseMapper.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2024-2024, the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,6 @@ package baby.mumu.basis.mappers;
 
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.util.Optional;
 
 /**
  * base mapper
@@ -28,7 +27,6 @@ import java.util.Optional;
 public interface BaseMapper {
 
   default LocalDateTime offsetDateTimeToLocalDateTime(OffsetDateTime offsetDateTime) {
-    return Optional.ofNullable(offsetDateTime)
-      .map(OffsetDateTime::toLocalDateTime).orElse(null);
+    return offsetDateTime != null ? offsetDateTime.toLocalDateTime() : null;
   }
 }
