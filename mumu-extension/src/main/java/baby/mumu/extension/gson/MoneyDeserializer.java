bc73e3a102a59593ea886d974a60920775a6f819
kaiyu.shan <kaiyu.shan@outlook.com>
2024-12-11 17:07:49 +0800

    refactor(extension): 更改Money Gson序列化和反序列化方式

--- mumu-extension/src/main/java/baby/mumu/extension/gson/MoneyDeserializer.java
+++ mumu-extension/src/main/java/baby/mumu/extension/gson/MoneyDeserializer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2024-2024, the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,8 +39,8 @@ public class MoneyDeserializer implements JsonDeserializer<Money> {
     return Optional.ofNullable(json).map(jsonNotNull -> {
       JsonObject jsonObject = json.getAsJsonObject();
       String currency = jsonObject.get("currency").getAsString();
-      BigDecimal amount = jsonObject.get("amount").getAsBigDecimal();
-      return Money.of(amount, currency);  // 使用币种和金额创建 Money 对象
+      String amountString = jsonObject.get("amount").getAsString();
+      return Money.of(new BigDecimal(amountString), currency);  // 使用币种和金额创建 Money 对象
     }).orElse(null);
   }
 }
--- mumu-extension/src/main/java/baby/mumu/extension/gson/MoneySerializer.java
+++ mumu-extension/src/main/java/baby/mumu/extension/gson/MoneySerializer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2024-2024, the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,7 +36,7 @@ public class MoneySerializer implements JsonSerializer<Money> {
     return Optional.ofNullable(money).map(moneyNotNull -> {
       JsonObject jsonObject = new JsonObject();
       jsonObject.addProperty("currency", moneyNotNull.getCurrency().getCurrencyCode());  // 币种
-      jsonObject.addProperty("amount", moneyNotNull.getNumberStripped());  // 金额
+      jsonObject.addProperty("amount", moneyNotNull.getNumberStripped().toPlainString());  // 金额
       return jsonObject;
     }).orElse(null);
   }
