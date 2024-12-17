ef063ae2e0aaae08906239f9682f6ef26d8484f7
kaiyu.shan <kaiyu.shan@outlook.com>
2024-12-17 19:42:40 +0800

    feat(mumu): 新增api说明文件

---
+++ apis/utils.js
@@ -0,0 +1,23 @@
+// noinspection all
+
+export function generateHeaders(serviceUrl) {
+  const requestIdString = $random.uuid
+  const timestampString = Date.now().toString()
+  const requestPath = request.url.tryGetSubstituted().replace(serviceUrl, "");
+  const requestBody = request.body.tryGetSubstituted() == null ? ''
+      : request.body.tryGetSubstituted();
+  // 转换为紧凑格式的 JSON 字符串
+  const compactJsonString = requestBody.length == 0 ? '' : JSON.stringify(
+      JSON.parse(requestBody));
+  const dataToSign = timestampString + requestIdString + requestPath
+      + compactJsonString;
+  const signatureString = crypto.hmac.sha256()
+  .withTextSecret(request.environment.get("secret"))
+  .updateWithText(dataToSign)
+  .digest().toHex();
+  return {
+    timestamp: timestampString,
+    requestId: requestIdString,
+    signature: signatureString,
+  };
+}
