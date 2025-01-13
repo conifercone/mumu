42e95bcc952f76978d7e92c3c9859d3ad503ef3e
kaiyu.shan <kaiyu.shan@outlook.com>
2025-01-13 10:07:05 +0800

    docs(basis): 调整注释

--- mumu-basis/src/main/java/baby/mumu/basis/po/package-info.java
+++ mumu-basis/src/main/java/baby/mumu/basis/po/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2024, the original author or authors.
+ * Copyright (c) 2024-2025, the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 /**
- * basis data objects
+ * persistent object
  *
  * @author <a href="mailto:kaiyu.shan@outlook.com">kaiyu.shan</a>
  * @since 1.0.0
