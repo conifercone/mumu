3aa86145cb52a98b19aee3655e616da84c9a797c
kaiyu.shan <kaiyu.shan@outlook.com>
2025-02-17 15:43:43 +0800

    refactor(mumu): 开启配置缓存

--- mumu-processor/src/main/java/baby/mumu/processor/metamodel/MetamodelGenerator.java
+++ mumu-processor/src/main/java/baby/mumu/processor/metamodel/MetamodelGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2024, the original author or authors.
+ * Copyright (c) 2024-2025, the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -105,7 +105,7 @@ public class MetamodelGenerator extends AbstractProcessor {
     osName = processingEnv.getOptions().get("os.name");
     projectVersion = processingEnv.getOptions().get("project.version");
     projectName = processingEnv.getOptions().get("project.name");
-    messager.printMessage(Diagnostic.Kind.NOTE, "🐏 MuMu Entity Metamodel Generator");
+    messager.printMessage(Diagnostic.Kind.NOTE, "🥐 MuMu Entity Metamodel Generator");
   }
 
   @Override
