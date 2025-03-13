0f3a33959dad214ae6e4b3cadfa84956e0b1dc76
kaiyu.shan <kaiyu.shan@outlook.com>
2025-03-13 14:14:49 +0800

    refactor(mumu): jdk升级至23

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
+            languageVersion.set(JavaLanguageVersion.of(23))
         }
     }
 
