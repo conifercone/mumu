fad6f2497fa606bab79bc5c7fbd2152879f36215
kaiyu.shan <kaiyu.shan@mumu.baby>
2025-03-28 14:46:05 +0800

    refactor(authentication): 用户语言偏好默认值修改为EN

---
+++ mumu-services/mumu-authentication/src/main/resources/db/migration/postgresql/V1.4.3__alert_columns_for_users_table.sql
@@ -0,0 +1,5 @@
+-- @formatter:off
+-- noinspection SqlConstantExpressionForFile
+-- noinspection SqlResolveForFile
+ALTER TABLE mumu_users ALTER COLUMN language SET DEFAULT 'EN';
+ALTER TABLE mumu_users_archived ALTER COLUMN language SET DEFAULT 'EN';
