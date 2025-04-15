128a6dac954d56a5ca17dd8ea821076fe5aac54d
kaiyu.shan <kaiyu.shan@outlook.com>
2025-04-15 11:55:13 +0800

    build(benchmark): 新增saveBenchmarkResult任务

--- mumu-benchmark/build.gradle.kts
+++ mumu-benchmark/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
 plugins {
     alias(libs.plugins.jmh)
 }
@@ -5,8 +8,25 @@ plugins {
 dependencies {
     jmh(libs.bundles.jmh)
 }
-
+val jmhReportFile = layout.buildDirectory.file("reports/jmh/result.json")
+val jmhHistoryDir = layout.projectDirectory.dir("../benchmark-history")
 jmh {
     resultFormat.set("JSON")
-    resultsFile.set(layout.buildDirectory.file("reports/jmh/result.json"))
+    resultsFile.set(jmhReportFile)
+}
+
+
+tasks.register<Copy>("saveBenchmarkResult") {
+    dependsOn("jmh")
+
+    val timestamp = SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Date())
+    val outputFileName = "result_$timestamp.json"
+
+    from(jmhReportFile)
+    into(jmhHistoryDir)
+    rename { outputFileName }
+
+    doLast {
+        println("✅ Benchmark result saved to benchmark-history/$outputFileName")
+    }
 }
