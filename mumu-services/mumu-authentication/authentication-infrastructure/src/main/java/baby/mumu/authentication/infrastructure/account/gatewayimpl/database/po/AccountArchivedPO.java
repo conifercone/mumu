7599f661285dbb0d2caba307504cb06424684776
kaiyu.shan <kaiyu.shan@outlook.com>
2025-05-12 16:57:20 +0800

    chore(authentication): 属性增加注释

--- mumu-services/mumu-authentication/authentication-infrastructure/src/main/java/baby/mumu/authentication/infrastructure/account/gatewayimpl/database/po/AccountArchivedPO.java
+++ mumu-services/mumu-authentication/authentication-infrastructure/src/main/java/baby/mumu/authentication/infrastructure/account/gatewayimpl/database/po/AccountArchivedPO.java
@@ -56,58 +56,103 @@ public class AccountArchivedPO extends JpaBasisArchivablePersistentObject {
   @Serial
   private static final long serialVersionUID = -7785479212845125722L;
 
+  /**
+   * 账户id
+   */
   @Id
   @Column(name = "id", nullable = false)
   private Long id;
 
+  /**
+   * 账户名
+   */
   @Size(max = 50)
   @Column(name = "username", nullable = false, length = 50)
   private String username;
 
+  /**
+   * 账户密码
+   */
   @Size(max = 500)
   @Column(name = "password", nullable = false, length = 500)
   private String password;
 
+  /**
+   * 已启用
+   */
   @Column(name = "enabled", nullable = false)
   private boolean enabled = true;
 
+  /**
+   * 凭证未过期
+   */
   @Column(name = "credentials_non_expired", nullable = false)
   private boolean credentialsNonExpired = true;
 
+  /**
+   * 帐户未锁定
+   */
   @Column(name = "account_non_locked", nullable = false)
   private boolean accountNonLocked = true;
 
+  /**
+   * 帐号未过期
+   */
   @Column(name = "account_non_expired", nullable = false)
   private boolean accountNonExpired = true;
 
+  /**
+   * 头像地址
+   */
   @Size(max = 200)
   @Column(name = "avatar_url", length = 200, nullable = false)
   private String avatarUrl;
 
+  /**
+   * 国际电话区号
+   */
   @Size(max = 10)
   @Column(name = "phone_country_code", length = 10, nullable = false)
   private String phoneCountryCode;
 
+  /**
+   * 手机号
+   */
   @Size(max = 200)
   @Column(name = "phone", length = 200, nullable = false)
   private String phone;
 
+  /**
+   * 性别
+   */
   @Column(name = "gender", nullable = false)
   @Enumerated(EnumType.STRING)
   private GenderEnum gender;
 
+  /**
+   * 电子邮箱
+   */
   @Size(max = 200)
   @Column(name = "email", length = 200, nullable = false)
   private String email;
 
+  /**
+   * 时区
+   */
   @Size(max = 200)
   @Column(name = "timezone", length = 200, nullable = false)
   private String timezone;
 
+  /**
+   * 语言偏好
+   */
   @Column(name = "language", nullable = false)
   @Enumerated(EnumType.STRING)
   private LanguageEnum language;
 
+  /**
+   * 出生日期
+   */
   @NotNull
   @Column(name = "birthday", nullable = false)
   private LocalDate birthday;
