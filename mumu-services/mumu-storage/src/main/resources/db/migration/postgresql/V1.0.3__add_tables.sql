5a23025e445f1729075b1ebf02e772055085d503
Kaiyu Shan <kaiyu.shan@outlook.com>
2025-07-02 15:51:54 +0800

    feat(storage): 新增mumu_file_storage_zones表 #228

---
+++ mumu-services/mumu-storage/src/main/resources/db/migration/postgresql/V1.0.3__add_tables.sql
@@ -0,0 +1,42 @@
+-- @formatter:off
+-- noinspection SqlConstantExpressionForFile
+-- noinspection SqlResolveForFile
+CREATE TABLE mumu_file_storage_zones
+(
+    id                BIGINT                                             NOT NULL
+        constraint mumu_file_storage_zones_pk
+            primary key,
+    code              varchar(255)                                       NOT NULL,
+    name              varchar(255)                                       NOT NULL default '',
+    description       varchar(500)                                       NOT NULL default '',
+    policy            varchar(255)                                       NOT NULL,
+    creation_time     timestamp with time zone default CURRENT_TIMESTAMP not null,
+    founder           bigint                   default 0                 not null,
+    modifier          bigint                   default 0                 not null,
+    modification_time timestamp with time zone default CURRENT_TIMESTAMP not null
+);
+comment
+    on table mumu_file_storage_zones is '文件存储区域';
+comment
+    on column mumu_file_storage_zones.id is '唯一标识符';
+comment
+    on column mumu_file_storage_zones.code is '编码';
+comment
+    on column mumu_file_storage_zones.name is '名称';
+comment
+    on column mumu_file_storage_zones.description is '描述信息';
+comment
+    on column mumu_file_storage_zones.policy is '策略';
+comment
+    on column mumu_file_storage_zones.creation_time is '创建时间';
+comment
+    on column mumu_file_storage_zones.founder is '创建人';
+comment
+    on column mumu_file_storage_zones.modifier is '修改人';
+comment
+    on column mumu_file_storage_zones.modification_time is '修改时间';
+ALTER TABLE mumu_file_metadata
+    RENAME COLUMN storage_zone TO storage_zone_id;
+ALTER TABLE mumu_file_metadata
+    ALTER
+        COLUMN storage_zone_id TYPE BIGINT USING storage_zone_id::BIGINT;
