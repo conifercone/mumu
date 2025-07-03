212a7af8b442268d91d8e03f39d9973d3f0b3fd9
Kaiyu Shan <kaiyu.shan@outlook.com>
2025-07-03 17:38:49 +0800

    refactor(storage): 优化sql脚本 #228

--- mumu-services/mumu-storage/src/main/resources/db/migration/postgresql/V1.0.3__add_tables.sql
+++ mumu-services/mumu-storage/src/main/resources/db/migration/postgresql/V1.0.3__add_tables.sql
@@ -15,28 +15,16 @@ CREATE TABLE mumu_file_storage_zones
     modifier          bigint                   default 0                 not null,
     modification_time timestamp with time zone default CURRENT_TIMESTAMP not null
 );
-comment
-    on table mumu_file_storage_zones is '文件存储区域';
-comment
-    on column mumu_file_storage_zones.id is '唯一标识符';
-comment
-    on column mumu_file_storage_zones.code is '编码';
-comment
-    on column mumu_file_storage_zones.name is '名称';
-comment
-    on column mumu_file_storage_zones.description is '描述信息';
-comment
-    on column mumu_file_storage_zones.policy is '策略';
-comment
-    on column mumu_file_storage_zones.creation_time is '创建时间';
-comment
-    on column mumu_file_storage_zones.founder is '创建人';
-comment
-    on column mumu_file_storage_zones.modifier is '修改人';
-comment
-    on column mumu_file_storage_zones.modification_time is '修改时间';
-ALTER TABLE mumu_file_metadata
-    RENAME COLUMN storage_zone TO storage_zone_id;
-ALTER TABLE mumu_file_metadata
-    ALTER
-        COLUMN storage_zone_id TYPE BIGINT USING storage_zone_id::BIGINT;
+comment on table mumu_file_storage_zones is '文件存储区域基本信息';
+comment on column mumu_file_storage_zones.id is '唯一标识符';
+comment on column mumu_file_storage_zones.code is '编码';
+comment on column mumu_file_storage_zones.name is '名称';
+comment on column mumu_file_storage_zones.description is '描述信息';
+comment on column mumu_file_storage_zones.policy is '策略';
+comment on column mumu_file_storage_zones.creation_time is '创建时间';
+comment on column mumu_file_storage_zones.founder is '创建人';
+comment on column mumu_file_storage_zones.modifier is '修改人';
+comment on column mumu_file_storage_zones.modification_time is '修改时间';
+COMMENT ON TABLE mumu_file_metadata IS '文件元数据基本信息';
+ALTER TABLE mumu_file_metadata RENAME COLUMN storage_zone TO storage_zone_id;
+ALTER TABLE mumu_file_metadata ALTER COLUMN storage_zone_id TYPE BIGINT USING storage_zone_id::BIGINT;
