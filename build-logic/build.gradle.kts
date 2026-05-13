7196e4cc53379a63c60fb5ebad23ed2597786495
Kaiyu Shan <kaiyu.shan@outlook.com>
2026-05-13 16:44:00 +0800

    feat(gradle): 统一定义位置

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -13,6 +13,6 @@ dependencies {
     implementation(libs.springboot.gradle.plugin)
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.lombok.gradle.plugin)
-    implementation("org.jetbrains.kotlin:kotlin-allopen:${libs.versions.kotlinPluginVersion.get()}")
-    implementation("org.jetbrains.kotlin:kotlin-noarg:${libs.versions.kotlinPluginVersion.get()}")
+    implementation(libs.kotlin.allopen)
+    implementation(libs.kotlin.noarg)
 }
