55480a633863cef675c16e814fb4fef030b2c9c2
Kaiyu Shan <kaiyu.shan@outlook.com>
2026-05-14 10:58:10 +0800

    feat(gradle): 服务聚合模块改用组合插件
    mumu-genix/mumu-iam/mumu-log/mumu-storage 根 build.gradle.kts 从
    6 个独立插件改为 mumu.service-conventions + mumu.springboot-conventions

--- mumu-services/mumu-genix/build.gradle.kts
+++ mumu-services/mumu-genix/build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
-    id("mumu.java-conventions")
-    id("mumu.kotlin-conventions")
-    id("mumu.quality-conventions")
-    id("mumu.publish-conventions")
-    id("mumu.processor-conventions")
+    id("mumu.service-conventions")
     id("mumu.springboot-conventions")
 }
 
